3f38cae6d3c6f5164decb4c9a33a13565e583522
Oliver Lietz <info@oliverlietz.de>
2012-03-06 22:36:49 +0100

    stick to schema1.xsd for now (see http://java.net/jira/browse/JAXB-743)

--- DiscCreationTools/net.java.bd.tools.index/src/main/java/net/java/bd/tools/index/package-info.java
+++
@@ -1,6 +0,0 @@
-@XmlSchema(
-    xmlns = {},
-    namespace = "http://index.tools.bd.java.net"
-) package net.java.bd.tools.index;
-
-import javax.xml.bind.annotation.XmlSchema;
--- DiscCreationTools/net.java.bd.tools.movieobject/src/main/java/net/java/bd/tools/movieobject/package-info.java
+++
@@ -1,6 +0,0 @@
-@XmlSchema(
-    xmlns = {},
-    namespace = "http://movieobject.tools.bd.java.net"
-) package net.java.bd.tools.movieobject;
-
-import javax.xml.bind.annotation.XmlSchema;
